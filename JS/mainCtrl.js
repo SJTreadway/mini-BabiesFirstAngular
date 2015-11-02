294da1e8cbf8eafdf6876a26c64d3f6a34122ee8
SJTreadway <treadway08@gmail.com>
2015-11-01 22:05:01 -0600

    completes extra credit

--- JS/mainCtrl.js
+++ JS/mainCtrl.js
@@ -4,4 +4,21 @@ app.controller('mainCtrl', function($scope) {
 
 	$scope.friends = ['Steve Jobs', 'Mark Zuckerberg', 'Bill Gates', 'Steve Wozniak'];
 
+	$scope.addFriends = function() {
+        if(!$scope.newFriend) {
+            return;
+        }
+
+        for (var i = 0; i < this.friends.length; i++) {
+        	if (this.friends[i] === this.newFriend) {
+        		alert("Friend already exists!");
+        		$scope.newFriend = '';
+        		return;
+        	}
+        }
+
+        $scope.friends.push($scope.newFriend);
+        $scope.newFriend = '';
+	}
+
 });
